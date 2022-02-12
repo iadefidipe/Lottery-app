4b26bdbcbf35444193d597dc0449ac95b63b35a1
Israel Adefidipe <israeladefidipe@gmail.com>
2022-02-12 15:33:08 +0100

    fix: app run with new react script fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css'
 import web3 from './web3'
 
 function App() {
-	web3.eth.getAccounts().then(console.log)
+	
 	return <div className='App'></div>
 }
 
--- src/web3.js
+++ src/web3.js
@@ -1,7 +1,7 @@
 import Web3 from 'web3'
 
-window.ethereum.request({ method: 'eth_requestAccounts' })
+window.ethereum.request({ method: 'eth_requestAccounts' }) //metamask provider
 
-const web3 = new Web3(window.ethereum)
+const web3 = new Web3(window.ethereum) // create a new instance of web3, injecting the provider from metamask
 
 export default web3
