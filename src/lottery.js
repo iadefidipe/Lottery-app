31068d5f963ab8736f0f0815d9d3b5abbc95da33
Israel Adefidipe <israeladefidipe@gmail.com>
2022-02-13 22:12:14 +0100

    client side lottery contract

--- src/lottery.js
+++ src/lottery.js
@@ -2,65 +2,47 @@ import web3 from "./web3"
 
 const address = '0x9dc12525cbec3f0b5a3e3d27753012eb78f10a93' //contract address
 
-const abi = [
+const abi =  [
   {
-    inputs: [],
-    stateMutability: "nonpayable",
-    type: "constructor",
-    constant: undefined,
-    payable: undefined,
-    signature: "constructor",
+      inputs: [], stateMutability: 'nonpayable', type: 'constructor'
   },
   {
-    inputs: [],
-    name: "enter",
-    outputs: [],
-    stateMutability: "payable",
-    type: "function",
-    constant: undefined,
-    payable: true,
-    signature: "0xe97dcb62",
+      inputs: [],
+      name: 'enter',
+      outputs: [],
+      stateMutability: 'payable',
+      type: 'function'
   },
   {
-    inputs: [],
-    name: "getPlayers",
-    outputs: [[Object]],
-    stateMutability: "view",
-    type: "function",
-    constant: true,
-    payable: undefined,
-    signature: "0x8b5b9ccc",
+      inputs: [],
+      name: 'getPlayers',
+      outputs: [{ internalType: 'address[]', name: '', type: 'address[]' }],
+      stateMutability: 'view',
+      type: 'function'
   },
   {
-    inputs: [],
-    name: "manager",
-    outputs: [[Object]],
-    stateMutability: "view",
-    type: "function",
-    constant: true,
-    payable: undefined,
-    signature: "0x481c6a75",
+      inputs: [],
+      name: 'manager',
+      outputs: [{ internalType: 'address', name: '', type: 'address' }],
+      stateMutability: 'view',
+      type: 'function'
   },
   {
-    inputs: [],
-    name: "pickWinner",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-    constant: undefined,
-    payable: undefined,
-    signature: "0x5d495aea",
+      inputs: [],
+      name: 'pickWinner',
+      outputs: [],
+      stateMutability: 'payable',
+      type: 'function'
   },
   {
-    inputs: [[Object]],
-    name: "players",
-    outputs: [[Object]],
-    stateMutability: "view",
-    type: "function",
-    constant: true,
-    payable: undefined,
-    signature: "0xf71d96cb",
-  },
+      inputs: [{
+          internalType: 'uint256', name: '', type: 'uint256'
+      }],
+      name: 'players',
+      outputs: [{ internalType: 'address', name: '', type: 'address' }],
+      stateMutability: 'view',
+      type: 'function'
+  }
 ]
 
-export default new web3.eth.Contract(abi, address)
+export default new web3.eth.Contract(abi, address) // local contract instance
