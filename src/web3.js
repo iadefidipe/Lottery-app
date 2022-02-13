690e3069806b72ce03732b9dcf58f626e06ee549
Israel Adefidipe <israeladefidipe@gmail.com>
2022-02-13 22:10:48 +0100

    web3 provider

--- src/web3.js
+++ src/web3.js
@@ -1,6 +1,6 @@
 import Web3 from 'web3'
 
-window.ethereum.request({ method: 'eth_requestAccounts' }) //metamask provider
+// window.ethereum.request({ method: 'eth_requestAccounts' }) //metamask provider
 
 const web3 = new Web3(window.ethereum) // create a new instance of web3, injecting the provider from metamask
 
